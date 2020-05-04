f9b578a634fab298296e0640e57a4ed579c54613
Alex Thomas <adthomas811@gmail.com>
2020-05-04 18:36:39 -0500

    Added tests folder with simple import test

---
+++ tests/test_import.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class AudacityScriptingTests(unittest.TestCase):
+   def test_import(self):
+      import audacity_scripting
+      import audacity_scripting.base
+      import audacity_scripting.utils
+
+      self.assertTrue(True)
