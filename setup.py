e23540937f993b713520b2181124ce47fcc9b050
Alex Thomas <adthomas811@gmail.com>
2020-05-05 14:04:46 -0500

    Updated indentation to 4 spaces.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
-   long_description = fh.read()
+    long_description = fh.read()
 
 setup(name='audacity-scripting',
       version="0.0.1",
--- tests/test_import.py
+++ tests/test_import.py
@@ -2,9 +2,8 @@ import unittest
 
 
 class AudacityScriptingTests(unittest.TestCase):
-   def test_import(self):
-      # import audacity_scripting
-      # import audacity_scripting.base
-      # import audacity_scripting.utils
-
-      self.assertTrue(True)
+    def test_import(self):
+        # import audacity_scripting
+        # import audacity_scripting.base
+        # import audacity_scripting.utils
+        self.assertTrue(True)
