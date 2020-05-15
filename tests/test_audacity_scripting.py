d3a1746ad0a55b508e46192291ae8d6952c766dd
Alex Thomas <adthomas811@gmail.com>
2020-05-14 20:38:16 -0500

    Fixed PEP8 issues.

--- tests/test_audacity_scripting.py
+++ tests/test_audacity_scripting.py
@@ -22,93 +22,95 @@ class AudacityScriptingTests(unittest.TestCase):
 # https://github.com/audacity/audacity/blob/master/lib-src/mod-script-pipe/PipeServer.cpp
 # http://timgolden.me.uk/pywin32-docs/win32pipe.html
 
-#import win32pipe
-#import win32file
+# import win32pipe
+# import win32file
 
 
-#class AudacityMock(object):
-#    def __init__(self):
-#        if sys.platform == 'win32':
-#            PIPE_REJECT_REMOTE_CLIENTS = 0x00000008
-#            open_mode = win32pipe.PIPE_ACCESS_DUPLEX
-#            pipe_mode = (win32pipe.PIPE_TYPE_MESSAGE |
-#                         win32pipe.PIPE_READMODE_MESSAGE |
-#                         win32pipe.PIPE_WAIT |
-#                         PIPE_REJECT_REMOTE_CLIENTS)
-#            max_instances = win32pipe.PIPE_UNLIMITED_INSTANCES
-#            buffer_size = 1024
+# class AudacityMock(object):
+#     def __init__(self):
+#         if sys.platform == 'win32':
+#             PIPE_REJECT_REMOTE_CLIENTS = 0x00000008
+#             open_mode = win32pipe.PIPE_ACCESS_DUPLEX
+#             pipe_mode = (win32pipe.PIPE_TYPE_MESSAGE |
+#                          win32pipe.PIPE_READMODE_MESSAGE |
+#                          win32pipe.PIPE_WAIT |
+#                          PIPE_REJECT_REMOTE_CLIENTS)
+#             max_instances = win32pipe.PIPE_UNLIMITED_INSTANCES
+#             buffer_size = 1024
 #
-#            self.tofile = win32pipe.CreateNamedPipe(r'\\.\pipe\ToSrvPipe',
-#                                                    open_mode,
-#                                                    pipe_mode,
-#                                                    max_instances,
-#                                                    buffer_size,
-#                                                    buffer_size,
-#                                                    50,
-#                                                    None)
-#            if self.tofile == win32file.INVALID_HANDLE_VALUE:
-#                # raise exception
-#                print('self.tofile not valid')
+#             self.tofile = win32pipe.CreateNamedPipe(r'\\.\pipe\ToSrvPipe',
+#                                                     open_mode,
+#                                                     pipe_mode,
+#                                                     max_instances,
+#                                                     buffer_size,
+#                                                     buffer_size,
+#                                                     50,
+#                                                     None)
+#             if self.tofile == win32file.INVALID_HANDLE_VALUE:
+#                 # raise exception
+#                 print('self.tofile not valid')
 #
-#            self.fromfile = win32pipe.CreateNamedPipe(r'\\.\pipe\FromSrvPipe',
-#                                                      open_mode,
-#                                                      pipe_mode,
-#                                                      max_instances,
-#                                                      buffer_size,
-#                                                      buffer_size,
-#                                                      50,
-#                                                      None)
-#            if self.fromfile == win32file.INVALID_HANDLE_VALUE:
-#                # raise exception
-#                print('self.fromfile not valid')
-#       else:
-#           # Linux and Mac not Implemented
-#           toname = '/tmp/audacity_script_pipe.to.' + str(os.getuid())
-#           fromname = '/tmp/audacity_script_pipe.from.' + str(os.getuid())
+#             self.fromfile = win32pipe.CreateNamedPipe(r'\\.\pipe\FromSrvPipe',
+#                                                       open_mode,
+#                                                       pipe_mode,
+#                                                       max_instances,
+#                                                       buffer_size,
+#                                                       buffer_size,
+#                                                       50,
+#                                                       None)
+#             if self.fromfile == win32file.INVALID_HANDLE_VALUE:
+#                 # raise exception
+#                 print('self.fromfile not valid')
+#         else:
+#             # Linux and Mac not Implemented
+#             toname = '/tmp/audacity_script_pipe.to.' + str(os.getuid())
+#             fromname = '/tmp/audacity_script_pipe.from.' + str(os.getuid())
+
+#     def run_pipe_server(self):
+#         if sys.platform == 'win32':
+#             connected = (win32pipe.ConnectNamedPipe(
+#                             self.tofile, None) == 0 and
+#                          win32pipe.ConnectNamedPipe(
+#                             self.fromfile, None) == 0)
+#             if connected:
+#                 try:
+#                     while(True):
+#                         success, data = win32file.ReadFile(self.tofile,
+#                                                            buffer_size,
+#                                                            None)
+#                         if success != 0:
+#                             # Raise Exception
+#                             print('Read Failed!')
+#                             break
 #
-#    def run_pipe_server(self):
-#        if sys.platform == 'win32':
-#            connected = (win32pipe.ConnectNamedPipe(self.tofile, None) == 0 and
-#                         win32pipe.ConnectNamedPipe(self.fromfile, None) == 0)
-#            if connected:
-#                try:
-#                    while(True):
-#                        success, data = win32file.ReadFile(self.tofile,
-#                                                        buffer_size,
-#                                                        None)
-#                        if success != 0:
-#                            # Raise Exception
-#                            print('Read Failed!')
-#                            break
-#    
-#                        command = data.decode().split('\r')[0]
-#                        response = evaluate_command(command)
-#    
-#                        success = win32file.WriteFile(self.fromfile,
-#                                                    (response +
-#                                                    '\n').encode())[0]
-#                        if success != 0:
-#                            # Raise Exception
-#                            print('Write Failed!')
-#                            break
-#                finally:
-#                    win32file.FlushFileBuffers(self.tofile)
-#                    win32pipe.DisconnectNamedPipe(self.tofile)
-#                    win32file.CloseHandle(self.tofile)
-#                    win32file.FlushFileBuffers(self.fromfile)
-#                    win32pipe.DisconnectNamedPipe(self.fromfile)
-#                    win32file.CloseHandle(self.fromfile)
-#        else:
-#            # Linux and Mac not Implemented
-#            pass
+#                         command = data.decode().split('\r')[0]
+#                         response = evaluate_command(command)
 #
-#    def evaluate_command(self, command):
-#        scripting_id_and_args_list = command.split(':')
+#                         success = win32file.WriteFile(self.fromfile,
+#                                                       (response +
+#                                                       '\n').encode())[0]
+#                         if success != 0:
+#                             # Raise Exception
+#                             print('Write Failed!')
+#                             break
+#                 finally:
+#                     win32file.FlushFileBuffers(self.tofile)
+#                     win32pipe.DisconnectNamedPipe(self.tofile)
+#                     win32file.CloseHandle(self.tofile)
+#                     win32file.FlushFileBuffers(self.fromfile)
+#                     win32pipe.DisconnectNamedPipe(self.fromfile)
+#                     win32file.CloseHandle(self.fromfile)
+#         else:
+#             # Linux and Mac not Implemented
+#             pass
+
+#     def evaluate_command(self, command):
+#         scripting_id_and_args_list = command.split(':')
 #
-#        if len(scripting_id_and_args_list) == 1:
-#            
-#        elif len(scripting_id_and_args_list) == 2:
-#            
-#        elif len(scripting_id_and_args_list) > 2:
-#            
-#        else:
+#         if len(scripting_id_and_args_list) == 1:
+#
+#         elif len(scripting_id_and_args_list) == 2:
+#
+#         elif len(scripting_id_and_args_list) > 2:
+#
+#         else:
